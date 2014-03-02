19d96ab7e778f52238749d0988e95bcdc569cf2d
Tony Dieu <tnydieu@gmail.com>
2014-03-02 14:50:12 +0100

    update login.php with comments

--- php/assignments/php-login/incl/login.php
+++ php/assignments/php-login/incl/login.php
@@ -1,9 +1,11 @@
 <?php
 if(isset($_POST['submit'])) {
+  // call Login() when user submit.
   Login();
 }
 
 function Login() {
+  // check if username or password is empty.
   $username = isset($_POST['username']) ? $_POST['username'] : false;
   $password = isset($_POST['password']) ? $_POST['password'] : false;
 
@@ -18,17 +20,21 @@ function Login() {
     }
   }
   else if ($username && (!($password))) {
+    // empty password field.
     echo "You didn't fill out your password.";
     return false;
   }
   else if ((!($username)) && $password) {
+    // empty username field.
     echo "You didn't fill out your username.";
     return false;
   }
   else if ((!($username)) && (!($password))) {
+    // empty username and password.
     echo "You didn't fill out your username and password.";
   }
   else {
+    // someting went wrong.
     echo "An unknown error occurred.";
   }
 }
